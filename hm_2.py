2ad6e2ceeb10498411171795af4ec7b899bc22dc
Elizabeth2332 <20240492@dcfe.ie>
2025-11-27 10:36:38 +0000

    changes

--- hm_2.py
+++ hm_2.py
@@ -6,25 +6,24 @@ screen = turtle.Screen()
 t = turtle.Turtle()
 
 
-# t.forward(50)
+# t.forward(20)
 # t.left(60)
-# t.forward(50)
+# t.forward(20)
 # t.left(-120)
-# t.forward(50)
+# t.forward(20)
 # t.left(60)
-# t.forward(50)
+# t.forward(20)
 
 
 def koch_side(n):
     if n == 0:
-        t.forward(50)
+        t.forward(20)
     else:
         for angle in [60, -120, 60, 0]:  
             koch_side(n - 1)
             t.left(angle)
 
 # koch_side(2)
-
 for _ in range(3):
     koch_side(3)
     t.right(120)
